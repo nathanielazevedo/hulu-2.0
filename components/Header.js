cc09c89e303a801aeffa41c96e32b476a033664e
nathanielazevedo <nathanielazevedo@hotmail.com>
2021-09-06 17:16:29 -0400

    image alt added

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import Image from "next/image"
-import HeaderItem from '../pages/HeaderItem';
-import {BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon} from "@heroicons/react/outline"
+/** @format */
+
+import React from "react";
+import Image from "next/image";
+import HeaderItem from "../pages/HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
-        <HeaderItem title='HOME' Icon={HomeIcon} />
-        <HeaderItem title='HOME' Icon={LightningBoltIcon} />
-        <HeaderItem title='HOME' Icon={BadgeCheckIcon} />
-        <HeaderItem title='HOME' Icon={CollectionIcon} />
-        <HeaderItem title='HOME' Icon={SearchIcon} />
-        <HeaderItem title='HOME' Icon={UserIcon} />
-
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="HOME" Icon={LightningBoltIcon} />
+        <HeaderItem title="HOME" Icon={BadgeCheckIcon} />
+        <HeaderItem title="HOME" Icon={CollectionIcon} />
+        <HeaderItem title="HOME" Icon={SearchIcon} />
+        <HeaderItem title="HOME" Icon={UserIcon} />
       </div>
       <Image
-        className='object-contain'
+        className="object-contain"
         src="https://links.papareact.com/ua6"
         width={200}
         height={100}
+        alt="hulu logo"
       />
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
